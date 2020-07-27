eab16ef9575dd568492f36175a309398ed683d49
Kevin Iturralde <keviniturralde3@gmail.com>
2020-07-27 16:05:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,7 +49,7 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {
-    railroads {
+    railroads: {
       rent_in_dollars: {
         one_piece_owned: 25,
         two_pieces_owned: 50,
